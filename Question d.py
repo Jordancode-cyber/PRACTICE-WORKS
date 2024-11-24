77d9e3ca3a787a5de90b100b4ed8cc9b838cd5b1
Jordancode-cyber <fetajordan6@gmail.com>
2024-11-24 18:01:48 +0300

    Create Question d.py

---
+++ Question d.py
@@ -0,0 +1,9 @@
+#This program checks if a string is the same as the input string
+
+def check_string():
+    is_string = False
+    if isinstance(is_string, str):
+        print("This string is the same.")
+    else:
+        print("This string is not the same.")
+    
